0948905fcaf99a4df19bf5bbac60742898c18de8
jciszewski <jciszewski@amusys.pl>
2023-11-23 01:05:31 +0100

    add searching

--- src/organisms/HomePageOrganism.tsx
+++ src/organisms/HomePageOrganism.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {InputData} from "../atoms/InputData";
 import {FlatList, View} from "react-native";
 import {useHomePageStore} from "../store/homePageStore";
@@ -7,11 +7,18 @@ import {CurrencyLabel} from "../molecules/CurrencyLabel";
 export const HomePageOrganism: FC<{}> = () => {
     const notificationSettings = useHomePageStore().notification_settings
 
+    const [searchedSettings, setSearchedSettings] = useState([])
+    const [searchText, setSearchText] = useState("")
+
+    useEffect(() => {
+        setSearchedSettings(notificationSettings.filter(id => `${id.currencySymbol}-${id.secondCurrencySymbol}`.toUpperCase().includes(searchText.toUpperCase())))
+    }, [searchText])
+
     return (
         <View style={{gap: 10, margin: 15}}>
-            <InputData placeholder={"Find"} value={""} isActive={true}/>
+            <InputData placeholder={"Find"} value={searchText} onChange={setSearchText} isActive={true}/>
             <FlatList keyExtractor={(id) => id.currencySymbol + "-" + id.secondCurrencySymbol}
-                      data={notificationSettings}
+                      data={searchedSettings}
                       renderItem={({item: currency}) => <CurrencyLabel currency={currency}/>}
                       scrollEnabled={false}/>
         </View>
